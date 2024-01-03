7cf957754bd88b98dbd9eeeccde689483a45f298
Alexandre <alexandre.detrindade@gmail.com>
2024-01-04 00:51:27 +0100

    Platform et swicht platform fait

---
+++ Assets/Scripts/SwitchObjectRed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
+
+
+public class SwitchObjectRed : MonoBehaviour
+{
+    public bool isActive = true ;
+    public GameObject ObjectRed;
+    public GameObject ObjectRedEmpty;
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(isActive == true)
+            {
+                ObjectRed.SetActive(true); 
+                ObjectRedEmpty.SetActive(false); 
+                isActive = false;
+            }
+            else
+            {
+                ObjectRed.SetActive(false); 
+                ObjectRedEmpty.SetActive(true); 
+                isActive = true;
+            }
+        }
+    }
+}
