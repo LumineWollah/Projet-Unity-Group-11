413b789673cb038a78fe4ca6f4406ab38cc9cc23
Alexandre <alexandre.detrindade@gmail.com>
2024-01-05 00:20:45 +0100

    Telepporter

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject teleportTarget;
+    public GameObject player;
+ 
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = teleportTarget.transform.position;
+    }
+}
