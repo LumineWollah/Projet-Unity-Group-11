6d243d83053f71695d24cd213801daf038cec59b
ethanG <ethangunzb@gmail.com>
2024-01-08 22:10:32 +0100

    bug echelle en cours de correction

--- Assets/Scripts/Echelle.cs
+++ Assets/Scripts/Echelle.cs
@@ -1,28 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Echelle : MonoBehaviour
 {
     public float Speed = 5f;
-    public float delaiReinitialisation = 5f;
+    public Rigidbody playerRigidBody;
 
     private void OnTriggerStay(Collider other)
     {
-
-            float mouvementVertical = Input.GetAxis("Vertical");
-            Vector3 mouvement = new Vector3(0f, mouvementVertical, 0f) * Speed * Time.deltaTime;
-            other.transform.Translate(mouvement);
-            
-            if (mouvementVertical == 0f)
-            {
-                StartCoroutine(ReinitialiserPositionVerticale(other.transform));
-            }
+        float mouvementVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(0f, mouvementVertical, 0f) * Speed * Time.deltaTime;
+        other.transform.Translate(mouvement);
+        
     }
     
-    private System.Collections.IEnumerator ReinitialiserPositionVerticale(Transform playerTransform)
-    {
-        yield return new WaitForSeconds(delaiReinitialisation);
-        
-        Vector3 nouvellePosition = new Vector3(playerTransform.position.x, 0f, playerTransform.position.z);
-        playerTransform.position = nouvellePosition;
-    }
 }
