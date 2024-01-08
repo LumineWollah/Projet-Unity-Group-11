88a20e0a4d241677695d8a6663efc135952786fb
ethanG <ethangunzb@gmail.com>
2024-01-08 21:15:50 +0100

    adding echelle and dashpads

---
+++ Assets/Scripts/DashPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DashPlatform : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    private Vector3 _directionIntent;
+    public float dashForce = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+            _directionIntent = transform.right;
+            playerRigidbody.AddForce(_directionIntent * dashForce, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Scripts/Echelle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Echelle : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float delaiReinitialisation = 5f;
+
+    private void OnTriggerStay(Collider other)
+    {
+
+            float mouvementVertical = Input.GetAxis("Vertical");
+            Vector3 mouvement = new Vector3(0f, mouvementVertical, 0f) * Speed * Time.deltaTime;
+            other.transform.Translate(mouvement);
+            
+            if (mouvementVertical == 0f)
+            {
+                StartCoroutine(ReinitialiserPositionVerticale(other.transform));
+            }
+    }
+    
+    private System.Collections.IEnumerator ReinitialiserPositionVerticale(Transform playerTransform)
+    {
+        yield return new WaitForSeconds(delaiReinitialisation);
+        
+        Vector3 nouvellePosition = new Vector3(playerTransform.position.x, 0f, playerTransform.position.z);
+        playerTransform.position = nouvellePosition;
+    }
+}
