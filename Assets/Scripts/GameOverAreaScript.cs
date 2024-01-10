b10ea54e7b1472a8d6ae5384838eafbc78a7a703
ethanG <ethangunzb@gmail.com>
2024-01-10 22:50:12 +0100

    final tutorial just without bot

--- Assets/Scripts/GameOverAreaScript.cs
+++ Assets/Scripts/GameOverAreaScript.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameOverAreaScript : MonoBehaviour
 {
+    public AudioClip sound;
     public Collider Player;
     private void OnTriggerEnter(Collider other)
     {
         if (QuakeLikeFPSScript.hp > 1) {
             QuakeLikeFPSScript.hp--;
+            AudioSource.PlayClipAtPoint(sound, transform.position);
         } else {
             if(other == Player){
                 Cursor.lockState = CursorLockMode.None;
