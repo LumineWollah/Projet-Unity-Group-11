27cad3adbb9960bba754acd922848e422f3aa611
ethanG <ethangunzb@gmail.com>
2024-01-10 23:51:10 +0100

    patching life bug (it was impossible to go under 1 pv)

--- Assets/Scripts/GameOverAreaScript.cs
+++ Assets/Scripts/GameOverAreaScript.cs
@@ -3,17 +3,15 @@ using UnityEngine.SceneManagement;
 public class GameOverAreaScript : MonoBehaviour
 {
     public AudioClip sound;
-    public Collider Player;
+    public Collider player;
     private void OnTriggerEnter(Collider other)
     {
-        if (QuakeLikeFPSScript.hp > 1) {
+        if (QuakeLikeFPSScript.hp >= 1) {
             QuakeLikeFPSScript.hp--;
             AudioSource.PlayClipAtPoint(sound, transform.position);
         } else {
-            if(other == Player){
                 Cursor.lockState = CursorLockMode.None;
                 SceneManager.LoadScene("YouLostMenu");
             }
         }
     }
-}
