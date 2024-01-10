87b41587d85997558e78b54907dfe7167373c63f
ethanG <ethangunzb@gmail.com>
2024-01-10 20:34:25 +0100

    Adding first sound script when player tutch bomb script named bomb

---
+++ Assets/Scripts/sound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BombeScript : MonoBehaviour
+{
+    public AudioClip explosionSound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("PlayerRoot"))
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+        }
+    }
+}
